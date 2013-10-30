a2ac2058627e531cb3fb9e540e0fa40327eca316
jsurya <jsurya@ualberta.ca>
2013-10-30 16:41:31 -0600

    Added StoryFragmentReadActivity

--- src/story/book/EditTextIllustration.java
+++ src/story/book/EditTextIllustration.java
@@ -17,43 +17,44 @@ import android.widget.Button;
  */
 
 public class EditTextIllustration extends Fragment {
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		final View rootView = inflater.inflate(R.layout.edit_text_illustration, container, false);
 		setTitle(R.string.FragmentTitle);
-		
+
 		// add VIEW to story handler
 		// StoryHandler sh = StoryHandlerApplication.getStoryHandler();
 		// sh.addView(this);
-		Button newButton = (Button) rootView.findViewById(R.id.Button_SaveEditText);
+		Button saveButton = (Button) rootView.findViewById(R.id.Button_SaveEditText);
 		String text = (String) editTextIllustration.toString();
-		newButton.setOnClickListener(new View.OnClickListener()
-		{
+		saveButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
 				//String textIllustration = (String) EditTextIllustration.toString();
-				
 				setContextText(text);
 				//saveText(textIllustration);
-			
-			}});
+			}
+		});
+	}
+
+	public void update() {
+		update();
 	}
 
 	private void setContentView(int editTextIllustration) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	private void setTitle(int fragmenttitle) {
+	private void setTitle(String fragmenttitle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	private void setContentText(){
+	private void setContentText(String text){
 		//StoryFragmentEditActivity.setText(text);
 	}
 }
---
+++ src/story/book/StoryFragmentReadActivity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package story.book;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author jsurya
+ *
+ */
+public class StoryFragmentReadActivity extends Activity {
+
+	/**
+	 * @param args
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.story_fragment_read_activity);
+	}
+
+}
