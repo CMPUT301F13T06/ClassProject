43e11958e4ba193a8df15c615c3309476e4181d4
Anthonyzou <azou@ualberta.ca>
2013-11-02 14:24:48 -0600

    Added getStoryList and getStoryInfoList methods in IOClient

--- src/story/book/IOClient.java
+++ src/story/book/IOClient.java
@@ -5,71 +5,118 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import android.content.Context;
+import android.util.Log;
 
-public class IOClient extends JSONClient{
+public class IOClient extends JSONClient {
 
-	private Context context;
+    private Context context;
 
-	/**
-	 * Unbuffered IO for writing a serialized story.
-	 * Buffered IO for read a serialized story string.
-	 * 
-	 * @param c the Context of the main application since
-	 * the package name directory can change
-	 */
-	public IOClient(Context c) {
-		super();
-		this.context = c;
-	}
+    /**
+     * Unbuffered IO for writing a serialized story. Buffered IO for read a
+     * serialized story string.
+     * 
+     * @param c
+     *            the Context of the main application since the package name
+     *            directory can change
+     */
+    public IOClient(Context c) {
+	super();
+	this.context = c;
+    }
 
-	/**
-	 * http://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
-	 * 
-	 * @param SID A fileID
-	 * @param aStory The story to be saved
-	 * @throws IOException IO exceptions should be caught in the calling code
-	 * since it doesn't make sense to try to handle it here.
-	 */
-	public void saveStory(Story aStory) throws IOException{
-		String serialStory = super.serializeStory(aStory);
-		FileOutputStream fos =  context.openFileOutput(String.valueOf(aStory.getStoryInfo().getSID()), 
-														Context.MODE_PRIVATE);
-		fos.write(serialStory.getBytes());
-		fos.flush();
-		fos.close();
+    /**
+     * http://stackoverflow.com/questions/14376807/how-to-read-write-string-from
+     * -a-file-in-android
+     * 
+     * @param SID
+     *            A fileID
+     * @param aStory
+     *            The story to be saved
+     * @throws IOException
+     *             IO exceptions should be caught in the calling code since it
+     *             doesn't make sense to try to handle it here.
+     */
+    public void saveStory(Story aStory) throws IOException {
+	String serialStory = super.serializeStory(aStory);
+	FileOutputStream fos = context.openFileOutput(
+		String.valueOf(aStory.getStoryInfo().getSID()),
+		Context.MODE_PRIVATE);
+	fos.write(serialStory.getBytes());
+	fos.flush();
+	fos.close();
+    }
+
+    /**
+     * 
+     * @param SID
+     *            the story to be deleted
+     * @return true on success and false on failure
+     */
+    public boolean deleteStory(int SID) {
+	// I add a slash here since getFilesDir doesnt include it at the end
+	return new File(context.getFilesDir() + "/" + String.valueOf(SID))
+		.delete();
+    }
+
+    /**
+     * 
+     * @return a list of all the SIDs on the internal device.
+     */
+    public ArrayList<String> getStoryList() {
+	ArrayList<String> listOfFileNames = new ArrayList<String>();
+	for (String temp : context.getFilesDir().list()) {
+	    listOfFileNames.add(temp);
 	}
-	
-	/**
-	 * 
-	 * @param SID the story to be deleted
-	 * @return true on success and false on failure
-	 */
-	public boolean deleteStory(int SID)
-	{
-		//I add a slash here since getFilesDir doesnt include it at the end
-		File file = new File(context.getFilesDir()+"/"+String.valueOf(SID));
-		return file.delete();
+	return listOfFileNames;
+    }
+
+    /**
+     * 
+     * @return an arraylist of StoryInfos
+     */
+    public ArrayList<StoryInfo> getStoryInfoList() {
+	ArrayList<StoryInfo> listOfStoryInfo = new ArrayList<StoryInfo>();
+	for (String file : getStoryList()) {
+	    Story aStory = null;
+	    try {
+		Log.d(file, "THE FILE");
+		aStory = getStory(Integer.valueOf(file).intValue());
+	    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    listOfStoryInfo.add(aStory.getStoryInfo());
 	}
-	/**
-	 * http://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
-	 * @param SID Likely to the SID of the story or any other string
-	 * @returns A string representing a serialized story;
-	 * @throws IOException IO exceptions should be caught in the calling code
-	 * since it doesn't make sense to try to handle it here.
-	 */
-	public Story getStory(int SID) throws IOException{
-		FileInputStream fis = context.openFileInput(String.valueOf(SID));
-		InputStreamReader isr = new InputStreamReader(fis);
-		char[] inputBuffer = new char[5048];
-		StringBuilder sb = new StringBuilder(5048); //set the initial size of string builder to be a decent sized length
-		int l;
-		while ((l = isr.read(inputBuffer)) != -1){
-			sb.append(inputBuffer, 0, l);
-		}
-		fis.close();
-		isr.close();
-		return super.unSerialize(sb.toString());
+	return listOfStoryInfo;
+    }
+
+    /**
+     * http://stackoverflow.com/questions/14376807/how-to-read-write-string-from
+     * -a-file-in-android
+     * 
+     * @param SID
+     *            Likely to the SID of the story or any other string
+     * @returns A string representing a serialized story;
+     * @throws IOException
+     *             IO exceptions should be caught in the calling code since it
+     *             doesn't make sense to try to handle it here.
+     */
+    public Story getStory(int SID) throws IOException {
+	FileInputStream fis = context.openFileInput(String.valueOf(SID));
+	InputStreamReader isr = new InputStreamReader(fis);
+	char[] inputBuffer = new char[5048];
+	StringBuilder sb = new StringBuilder(5048); // set the initial size of
+						    // string builder to be a
+						    // decent sized length
+	int l;
+	while ((l = isr.read(inputBuffer)) != -1) {
+	    sb.append(inputBuffer, 0, l);
 	}
+	fis.close();
+	isr.close();
+	return super.unSerialize(sb.toString());
+    }
 }
--- src/story/book/test/IOClientTest.java
+++ src/story/book/test/IOClientTest.java
@@ -1,5 +1,6 @@
 package story.book.test;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Test;
@@ -10,45 +11,51 @@ import story.book.Story;
 import story.book.StoryInfo;
 import android.test.ActivityInstrumentationTestCase2;
 
-public class IOClientTest extends ActivityInstrumentationTestCase2<story.book.MainActivity>{
-	Story s;
-	StoryInfo info;
-	public IOClientTest() {
-		super(MainActivity.class);
-		 {
-			info = new StoryInfo();
-			info.setAuthor("Daniel");
-			info.setTitle("Broken Star");
-			info.setGenre("Science Fiction");
-			info.setSynopsis("The princess of a destroyed kingdom is left with no one to guide her, until she finds a fallen star with a secret inside....");
-			info.setPublishDate(new Date());
-			info.setSID(600);
-			
-			s = new Story(info);
-		}
+public class IOClientTest extends
+	ActivityInstrumentationTestCase2<story.book.MainActivity> {
+    Story s;
+    StoryInfo info;
+
+    public IOClientTest() {
+	super(MainActivity.class);
+	{
+	    info = new StoryInfo();
+	    info.setAuthor("Daniel");
+	    info.setTitle("Broken Star");
+	    info.setGenre("Science Fiction");
+	    info.setSynopsis("The princess of a destroyed kingdom is left with no one to guide her, until she finds a fallen star with a secret inside....");
+	    info.setPublishDate(new Date());
+	    info.setSID(600);
+
+	    s = new Story(info);
 	}
+    }
 
+    @Test
+    public void testIOClient() throws Exception {
+	IOClient io = new IOClient(getActivity());
+	io.saveStory(s);
+	StoryInfo sample_info = io.getStory((s.getStoryInfo().getSID()))
+		.getStoryInfo();
 
-	@Test
-	public void testIOClient() throws Exception{
-		IOClient io = new IOClient(getActivity());
-		io.saveStory(s);
-		
-		StoryInfo sample_info = io.getStory((s.getStoryInfo().getSID())).getStoryInfo();
-		
-		assertEquals(sample_info.getAuthor() , ("Daniel"));
-		assertTrue(sample_info.getTitle().equals(info.getTitle()));
-		assertTrue(sample_info.getAuthor().equals(info.getAuthor()));
-		assertTrue(sample_info.getGenre().equals(info.getGenre()));
-		assertTrue(sample_info.getSynopsis().equals(info.getSynopsis()));
-		assertEquals(sample_info.getSID(), info.getSID());
-		
-		assertTrue(io.deleteStory(600));
-		assertFalse(io.deleteStory(600));
-		
-//		io.saveStory(null);
-//		assertEquals(io.getStory(600), null);
-		
+	assertEquals(sample_info.getAuthor(), ("Daniel"));
+	assertTrue(sample_info.getTitle().equals(info.getTitle()));
+	assertTrue(sample_info.getAuthor().equals(info.getAuthor()));
+	assertTrue(sample_info.getGenre().equals(info.getGenre()));
+	assertTrue(sample_info.getSynopsis().equals(info.getSynopsis()));
+	assertEquals(sample_info.getSID(), info.getSID());
 
-	}
+	assertEquals(io.getStoryList().get(0), ("600"));
+
+	ArrayList<StoryInfo> storyinfos = io.getStoryInfoList();
+	assertEquals(storyinfos.get(0).getAuthor(), "Daniel");
+
+	assertTrue(io.deleteStory(600));
+
+	assertFalse(io.deleteStory(600));
+
+	// io.saveStory(null);
+	// assertEquals(io.getStory(600), null);
+
+    }
 }
