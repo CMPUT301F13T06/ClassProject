f3a7ce380a00a22c2a79abd3c65b6c81f4dc8a1b
jsurya <jsurya@ualberta.ca>
2013-11-02 15:14:42 -0600

    Functional dashboard menu icon

--- src/story/book/StoryFragmentReadActivity.java
+++ src/story/book/StoryFragmentReadActivity.java
@@ -7,9 +7,11 @@ import android.app.ActionBar;
 import android.app.Fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * @author jsurya
@@ -20,30 +22,31 @@ public class StoryFragmentReadActivity extends Activity implements StoryView<Sto
     ActionBar.Tab Tab1, Tab2;
     Fragment readingTab1 = new ReadingFragment();
     Fragment annotationsTab2 = new AnnotationFragment();
-	
+    ActionBar actionBar;
+	//StoryFragment sf = new StoryFragment(fragmentTitle);
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.story_fragment_read_activity);
-		
-		ActionBar actionBar = getActionBar();
+		//R.string.FragmentTitle = sf.getFragmentTitle();
+		actionBar = getActionBar();
+		actionBar.setTitle(R.string.FragmentTitle);
 		 
         // Create Actionbar Tabs
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
  
         // Set Tab Icon and Titles
-        Tab1 = actionBar.newTab().setText(R.string.FragmentTitle);
+        Tab1 = actionBar.newTab().setText("Story");
         Tab2 = actionBar.newTab().setText("Annotations");
-        
  
         // Set Tab Listeners
         Tab1.setTabListener(new TabListener(readingTab1));
         Tab2.setTabListener(new TabListener(annotationsTab2));
  
-        // Add tabs to actionbar
+        // Add tabs to action bar
         actionBar.addTab(Tab1);
         actionBar.addTab(Tab2);
-        
 
 	}
 	
@@ -53,6 +56,19 @@ public class StoryFragmentReadActivity extends Activity implements StoryView<Sto
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.standard_menu, menu);
 	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.activity_main:
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+            
+        default:
+            return super.onOptionsItemSelected(item);
+        }
 	}
 
 	@Override
