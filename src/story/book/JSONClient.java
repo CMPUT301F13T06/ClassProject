c6145046955fa1d1f38dafbf6cda510e5dd2a5ac
Anthonyzou <azou@ualberta.ca>
2013-11-03 10:21:24 -0700

    Added author name tag

--- src/story/book/JSONClient.java
+++ src/story/book/JSONClient.java
@@ -1,32 +1,37 @@
 package story.book;
 
-import java.lang.reflect.Type;
-
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
+/**
+ * 
+ * @author Anthony Ou
+ * 
+ */
 public abstract class JSONClient {
-	
-	protected static Gson Gsonclient = new Gson();
-	
-	protected JSONClient() {}
-	
-	/**
-	 * 
-	 * @param an object
-	 * @return object as a serialized string.
-	 */
-	protected String serialize(Object data){
-		return Gsonclient.toJson(data); 
-	}
-	
-	/**
-	 * 
-	 * @param Serial is a string of serialized object of Type T
-	 * @return a null on failure or an object of Type T
-	 */
-	protected <T> T unSerialize(String serial, Class<T> type){
-		return Gsonclient.fromJson(serial, type);
-	}
+
+    protected static Gson Gsonclient = new Gson();
+
+    protected JSONClient() {
+    }
+
+    /**
+     * 
+     * @param an
+     *            object
+     * @return object as a serialized string.
+     */
+    protected String serialize(Object data) {
+	return Gsonclient.toJson(data);
+    }
+
+    /**
+     * 
+     * @param Serial
+     *            is a string of serialized object of Type T
+     * @return a null on failure or an object of Type T
+     */
+    protected <T> T unSerialize(String serial, Class<T> type) {
+	return Gsonclient.fromJson(serial, type);
+    }
 
 }
