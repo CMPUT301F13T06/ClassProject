a89d71a32bb12b3fd26b44e1613aac295086f871
jsurya <jsurya@ualberta.ca>
2013-11-04 19:27:22 -0700

    Story fragment list activity added

---
+++ src/story/book/StoryFragmentListActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package story.book;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Jessica Surya
+ *
+ */
+public class StoryFragmentListActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.story_fragment_read_activity);
+		
+	}
+}
