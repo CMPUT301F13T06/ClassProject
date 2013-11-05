9ccdb004ee45ac7f48689e6db66b52303a005f75
jsurya <jsurya@ualberta.ca>
2013-11-04 21:48:29 -0700

    Attempted to add button to CAB, still needs work

--- src/story/book/AnnotationFragment.java
+++ src/story/book/AnnotationFragment.java
@@ -3,11 +3,16 @@
  */
 package story.book;
 
+import java.util.zip.Inflater;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,7 +30,19 @@ public class AnnotationFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 		View rootView = inflater.inflate(R.layout.annotation_fragment, container, false);
+		
 		return rootView;
+	}
+	
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+	    // Inflate the menu items for use in the action bar
+		
+		inflater.inflate(R.menu.add_illustration_menu, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	   
 	}
 }
