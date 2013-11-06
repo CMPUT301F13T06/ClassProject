bd20f1bea2dae68c62127fe3417e0429eac0cf37
Anthonyzou <azou@ualberta.ca>
2013-11-05 17:24:13 -0700

    did the general layout of add decision branch popup dialog

--- src/story/book/AddDecisionBranch.java
+++ src/story/book/AddDecisionBranch.java
@@ -3,32 +3,38 @@
  */
 package story.book;
 
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.support.v4.app.DialogFragment;
 
 /**
  * @author jsurya
- * 
+ * @author anthony
  */
-public class AddDecisionBranch extends Fragment {
-	private ActionBar actionBar;
-	private LinearLayout list;
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		super.onCreate(savedInstanceState);
-		final View rootView = inflater.inflate(R.layout.add_decision_branch_fragment, container, false);
-		actionBar.setTitle(R.string.FragmentTitle);
-		
-		
-		list = (LinearLayout)this.getView().findViewById(R.id.allDecisionBranches);
-		return rootView;	
-	}
-	
+public class AddDecisionBranch extends DialogFragment {
+    
+    /**
+     * http://developer.android.com/guide/topics/ui/dialogs.html
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+	AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	builder.setTitle("Set Decision Branch Label");
+	builder.setMessage("Set decision")
+	.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+		//TODO setting the decision
+	    }
+	})
+	.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+		// User cancelled the dialog
+	    }
+	});
+	// Create the AlertDialog object and return it
+	return builder.create();
+    }
+
 }
