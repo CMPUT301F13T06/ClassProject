9a3126058c3e71a6d59b13a2928b02a315b7e2b8
jsurya <jsurya@ualberta.ca>
2013-11-06 18:10:32 -0700

    Opens the starting fragment from story in StoryApplication by default, illustrations are overlapped (TO FIX)

--- src/story/book/StoryFragmentEditActivity.java
+++ src/story/book/StoryFragmentEditActivity.java
@@ -3,15 +3,27 @@
  */
 package story.book;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import android.annotation.TargetApi;
 import android.app.ActionBar;
+import android.app.ActionBar.LayoutParams;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * StoryFragmentEditActivity is the interface users can make changes
@@ -21,68 +33,148 @@ import android.view.MenuItem;
  * @author jsurya
  *
  */
-@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class StoryFragmentEditActivity extends FragmentActivity {
-    ActionBar actionBar;
-    //FragmentCreationController FCC = new FragmentCreationController(storyFragment);
-    //storyFragment passed as intent from FragmentList
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.story_fragment_read_activity);
-	actionBar = getActionBar();
-	actionBar.setTitle(R.string.FragmentTitle);
-    }
-
-    @Override
-    public void onPause(){
-	super.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu items for use in the action bar
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.add_illustration_menu, menu);
-
-	inflater.inflate(R.menu.standard_menu, menu);
-	return super.onCreateOptionsMenu(menu);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-	// Handle item selection
-	switch (item.getItemId()) {
-	case R.id.text:
-	    
-	    return true;
-	case R.id.take_photo:
-	    
-	    return true;
-	case R.id.addGalleryPhoto:
-	    
-	    return true;
-	case R.id.addDecisionBranch:
-	    
-	    return true;
+	ActionBar actionBar;
 	
-	case R.id.audio:
-	    
-	    return true;
-	    
-	case R.id.video:
-	    
-	    return true;
-	    
-	case R.id.record_video:
-	    
-	    return true;
-	case R.id.title_activity_dashboard:
-	    Intent intent = new Intent(this, Dashboard.class);
-	    startActivity(intent);
-	    return true;
-
-	default:
-	    return super.onOptionsItemSelected(item);
+	StoryFragment SF;
+	FragmentCreationController FCC;
+	ArrayList<StoryFragment> SFL;
+	ArrayAdapter<StoryFragment> adapter;
+	StoryCreationController SCC;
+	StoryApplication SA;
+	
+	ArrayList<Illustration> illustrations;
+	ArrayList<DecisionBranch> decisions;
+	ArrayList<Button> buttons;
+	
+	//storyFragment passed as intent from FragmentList
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.story_fragment_read_activity);
+		
+		SCC = new StoryCreationController();
+		SFL = SCC.getFragments();
+		SA = new StoryApplication();
+		SF = SFL.get(0);
+		FCC = new FragmentCreationController(SF);
+		String title = SF.getFragmentTitle();
+		
+		actionBar = getActionBar();
+		actionBar.setTitle(title);
+		
 	}
-    }
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		illustrations = SF.getIllustrations();
+		decisions = SF.getDecisionBranches();
+
+		ArrayList<View> illustrationViews = new ArrayList<View>();
+
+		for (Illustration<?> i : illustrations){
+			illustrationViews.add(i.getView());
+		}
+
+		formatView(illustrationViews);
+		LayoutParams p = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT); 
+		for (View t: illustrationViews) {
+			this.addContentView(t, p);
+		}
+
+		buttons = formatButton(decisions, this);
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, 
+				LayoutParams.WRAP_CONTENT);
+
+		lp.addRule(RelativeLayout.ALIGN_BOTTOM);
+		lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+		for (Button dbButton : buttons) {
+			this.addContentView(dbButton, lp);
+		}
+
+	}
+
+	@Override
+	public void onPause(){
+		super.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.add_illustration_menu, menu);
+
+		inflater.inflate(R.menu.standard_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.text:
+
+			return true;
+		case R.id.take_photo:
+
+			return true;
+		case R.id.addGalleryPhoto:
+
+			return true;
+		case R.id.addDecisionBranch:
+
+			return true;
+
+		case R.id.audio:
+
+			return true;
+
+		case R.id.video:
+
+			return true;
+
+		case R.id.record_video:
+
+			return true;
+		case R.id.title_activity_dashboard:
+			Intent intent = new Intent(this, Dashboard.class);
+			startActivity(intent);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void formatView(ArrayList<View> v) {
+		Iterator<View> viewIterator = v.iterator();
+		TextView x = null;
+		while(viewIterator.hasNext()) {
+			x = (TextView) viewIterator.next();
+			x.setTextSize(20);
+			x.setTextColor(Color.BLACK);
+			x.setPaddingRelative(5, 0, 0, 0);
+		}
+	}
+
+	private ArrayList<Button> formatButton(ArrayList<DecisionBranch> db, Context c) {
+
+		ArrayList<Button> buttonList = new ArrayList<Button>();
+
+		Iterator<DecisionBranch> dbIterator = db.iterator();
+		DecisionBranch d = null;
+		Button button;
+		while(dbIterator.hasNext()) {
+
+			d = dbIterator.next();
+			button = new Button(c);
+			button.setText(d.getDecisionText());
+			buttonList.add(button);
+		}
+
+		return buttonList;
+	}
 }
