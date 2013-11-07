aaf1748724d2c744f4a4f1862cbbb6acf0795391
Alexander Cheung <atcheung@ualberta.ca>
2013-11-06 21:12:18 -0700

    Initial commit

---
+++ src/story/book/test/TextIllustrationTest.java
@@ -0,0 +1,42 @@
+package story.book.test;
+
+import org.junit.Test;
+
+import story.book.Dashboard;
+import story.book.TextIllustration;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TextIllustrationTest extends ActivityInstrumentationTestCase2
+		<story.book.Dashboard> {
+	
+	private TextIllustration textIllustration;
+	
+	public TextIllustrationTest() {
+		super(Dashboard.class);
+		textIllustration = new TextIllustration("Test illustration String");
+	}
+	
+	@Test
+	public void testCreation() {
+		textIllustration = new TextIllustration("Test illustration String");
+		assertNotNull(textIllustration);
+	}
+	
+	@Test
+	public void testGetContent() {
+		assertEquals(textIllustration.getContent(), 
+				"Test illustration String");
+	}
+
+	@Test
+	public void testSetContent() {
+		textIllustration.setContent("New test String");
+		assertEquals(textIllustration.getContent(), "New test String");
+	}
+	
+	@Test
+	public void testGetView() {
+		assertNotNull(textIllustration.getView());
+	}
+	
+}
