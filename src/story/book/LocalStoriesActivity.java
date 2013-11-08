75062825d7b415f469929504b3b11bec3950d1ff
Nancy Pham-Nguyen <nphamngu@ualberta.ca>
2013-11-07 22:43:52 -0700

    added documentation

--- src/story/book/LocalStoriesActivity.java
+++ src/story/book/LocalStoriesActivity.java
@@ -1,7 +1,6 @@
 package story.book;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,32 +13,23 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
+
 
 /**
- * Activity that allows the user to create new stories or select a story to read
- * from their local stories
- * 
+ * Activity that allows the user to create new stories, delete stories, edit stories, 
+ * or select a story to read from a list of their locally stored stories.
+ * This activity uses a controller LocalStoryController to get the story information
+ * to display in the list. The controller is also used to save stories.
+ *
  * @author Nancy Pham-Nguyen
  * @author Anthony Ou
  */
 public class LocalStoriesActivity extends Activity implements StoryView<Story> {
 
-	public static final String KEY_TITLE = "Title";
-	public static final String KEY_ID = "id";
-
 	ListView listView;
-
-	SimpleAdapter sAdapter;
-
-	ArrayList<HashMap<String, String>> sList;
-	HashMap<String, String> testMap;
-
 
 	private LocalStoryController localController;
 	ArrayAdapter<StoryInfo> a;
@@ -50,27 +40,12 @@ public class LocalStoriesActivity extends Activity implements StoryView<Story> {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.library_activity);
 
-		// localController.getStory();
-
 		localController = new LocalStoryController();
 
-		sList = new ArrayList<HashMap<String, String>>();
-
-		String[] from = new String[] { KEY_TITLE, KEY_ID };
-		int[] to = new int[] { R.id.textTest, R.id.textTest2 };
-
-		// for (int i = 0; i < from.length; i++) {
-		testMap = new HashMap<String, String>();
-		testMap.put(KEY_TITLE, from[0]);
-		testMap.put(KEY_ID, "" + "\n" + "hello");
-		sList.add(testMap);
-		// }
-
-		sAdapter = new SimpleAdapter(this, sList, R.layout.stories_list, from,
-				to);
-
 		listView = (ListView) findViewById(R.id.listView);
-		a = new ArrayAdapter<StoryInfo>(this, android.R.layout.simple_list_item_1, localController.getStoryList());
+		a = new ArrayAdapter<StoryInfo>(this,
+				android.R.layout.simple_list_item_1,
+				localController.getStoryList());
 		listView.setAdapter(a);
 
 		registerForContextMenu(listView);
@@ -118,14 +93,13 @@ public class LocalStoriesActivity extends Activity implements StoryView<Story> {
 	 */
 	public void deleteStory() {
 		// if long click on story then give option to delete
-		//SID = this.SID;
-		localController.deleteStory(position);
+		localController.deleteStory(a.getItem(position).getSID());
+		a.notifyDataSetChanged();
 	}
 
 	private void openSearch() {
 
 	}
-
 
 	/**
 	 * Method to create a floating context menu when an item in the list is
