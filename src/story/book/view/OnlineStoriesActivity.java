0950fd65dae24de98882f2b52a728b1f762ddb2f
vina1 <vina1@ualberta.ca>
2013-11-21 22:50:00 -0700

    update online lucky button to work with new adapter

--- src/story/book/view/OnlineStoriesActivity.java
+++ src/story/book/view/OnlineStoriesActivity.java
@@ -18,6 +18,7 @@
 package story.book.view;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import story.book.view.R;
@@ -27,8 +28,9 @@ import story.book.model.Story;
 import story.book.model.StoryInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,7 +39,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.SimpleAdapter;
 import android.support.v4.app.NavUtils;
 
 /**
@@ -50,11 +55,17 @@ import android.support.v4.app.NavUtils;
  */
 public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 
-	private StoryController onlineController;
-	ArrayList<StoryInfo> storyInfo;
 	ListView listView;
-
-	protected ArrayAdapter<StoryInfo> adapter;
+	
+	ArrayList<StoryInfo> storyInfo;
+	
+	ArrayList<HashMap<String, String>> sList;
+	SimpleAdapter sAdapter;
+	
+	SearchView searchView;
+	
+	private OnlineStoryController onlineController;
+	//protected ArrayAdapter<StoryInfo> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,12 +73,19 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 		setContentView(R.layout.library_activity);
 
 		onlineController = new OnlineStoryController();
-
-		adapter = new ArrayAdapter<StoryInfo>(this, android.R.layout.simple_list_item_1, onlineController.getStoryList());
+		
+		sList = new ArrayList<HashMap<String,String>>();
+		
+		
+		
+		
+		
+		//adapter = new ArrayAdapter<StoryInfo>(this, android.R.layout.simple_list_item_1, onlineController.getStoryList());
 		listView = (ListView) findViewById(R.id.listView);
+		refreshList(onlineController.getStoryList());
 		registerForContextMenu(listView);
 
-		listView.setAdapter(adapter);
+		
 
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			/*
@@ -76,7 +94,7 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 			@Override
 			public void  onItemClick
 			(AdapterView<?> parent , View view, int pos, long id) {
-				onlineController.getStory(adapter.getItem(pos).getSID());
+				onlineController.getStory(getFromAdapter(pos));
 				readStory();
 			}});
 
@@ -84,8 +102,9 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 		luckyButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if(adapter.getCount() > 0) {
-					onlineController.getStory(adapter.getItem( new Random().nextInt(adapter.getCount())).getSID());
+				if(sAdapter.getCount() > 0) {
+					int index =  new Random().nextInt(sAdapter.getCount());
+					onlineController.getStory(getFromAdapter(index));
 					readStory();
 				}
 			}
@@ -94,7 +113,38 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
+	
+	 /**
+     * Displays the list of local stories.
+     */
+    public void refreshList(ArrayList<StoryInfo> storyList) {
+            sList.clear();
 
+            for (StoryInfo storyInfo : storyList) {
+                    HashMap<String, String> item = new HashMap<String, String>();
+
+                    item.put("Title", storyInfo.getTitle());
+                    item.put("Author", storyInfo.getAuthor());
+                    item.put("Date", storyInfo.getPublishDateString());
+                    item.put("SID", String.valueOf(storyInfo.getSID()));
+
+                    sList.add(item);
+
+            }
+            
+
+    		String[] from = new String[] {"Title", "Author", "Date", "SID"};
+    		int[] to = new int[] {R.id.listItem1, R.id.listItem2, R.id.listItem3};
+    		
+    		sAdapter = new SimpleAdapter(this, sList, R.layout.stories_list, from, to);
+    		listView.setAdapter(sAdapter);
+    		
+    }
+
+    private int getFromAdapter(int pos){
+    	return Integer.parseInt(((HashMap<String,String>) sAdapter.getItem(pos)).get("SID"));
+    }
+    
 	/**
 	 * Method that is called when a user chooses to read the story
 	 * 
@@ -109,8 +159,9 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 	@Override
 	public void onResume() {
 		super.onResume();
-		adapter.clear();
-		adapter.addAll(onlineController.getStoryList());
+		//adapter.clear();
+		//adapter.addAll(onlineController.getStoryList());
+		refreshList(onlineController.getStoryList());
 	}
 
 	/**
@@ -120,29 +171,81 @@ public class OnlineStoriesActivity extends Activity implements StoryView<Story>{
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
+	
+	/**
+	 * http://stackoverflow.com/questions/18832890/android-nullpointerexception-on-searchview-in-action-bar
+	 * http://stackoverflow.com/questions/17874951/searchview-onquerytextsubmit-runs-twice-while-i-pressed-once
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.online_stories, menu);
+		getMenuInflater().inflate(R.menu.local_stories, menu);
+
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		 searchView = (SearchView) menu.findItem(R.id.action_search)
+				.getActionView();
+		// Assumes current activity is the searchable activity
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getComponentName()));
+		searchView.setIconifiedByDefault(true); //iconify the widget
+		searchView.setSubmitButtonEnabled(true);
+		
+			
+		handleSearch();
 		return true;
 	}
+	
+	private void searchResults(String query){	
+		//adapter.clear();
+		//show the list with just the search results
+		//adapter = new ArrayAdapter<StoryInfo>(this, android.R.layout.simple_list_item_1, onlineController.search(query));
+		//listView.setAdapter(adapter);
+		//refreshList(onlineController.getStoryList());
+		refreshList(onlineController.search(query));
+	}
+	
+	
+	private void handleSearch(){
+		searchView.setOnQueryTextListener(new OnQueryTextListener(){
+			
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				// TODO Auto-generated method stub
+				if(newText.isEmpty()){
+					//adapter.addAll(onlineController.getStoryList());
+					refreshList(onlineController.getStoryList());
+					return true;
+				}
+				else{
+				return false;
+				}
+			}
+			
 
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				// TODO Auto-generated method stub
+				//Do something when the user selects the submit button
+				
+			
+				searchResults(query);
+				
+				return true;
+			}
+		});
+		
+	}
+	
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.action_search:
-			openSearch();
 		case R.id.title_activity_dashboard:
 			NavUtils.navigateUpFromSameTask(this);
 			finish();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	private void openSearch() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
