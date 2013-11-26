1643acad3b957cabde49523610932b5bc1d5c525
Alexander Cheung <atcheung@ualberta.ca>
2013-11-26 13:33:51 -0700

    Initial commit for StoryInfoTest with test stubs

---
+++ src/story/book/test/StoryInfoTest.java
@@ -0,0 +1,109 @@
+package story.book.test;
+
+import org.junit.Test;
+
+import story.book.view.Dashboard;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class StoryInfoTest extends ActivityInstrumentationTestCase2
+<story.book.view.Dashboard> {
+
+	public StoryInfoTest() {
+		super(Dashboard.class);
+	}
+	
+	@Test
+	public void testCreation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetAuthor() {
+		
+	}
+	
+	@Test
+	public void testSetAuthor() {
+		
+	}
+	
+	@Test
+	public void testGetTitle() {
+		
+	}
+	
+	@Test
+	public void testSetTitle() {
+		
+	}
+
+	@Test
+	public void testGetGenre() {
+		
+	}
+	
+	@Test
+	public void testSetGenre() {
+		
+	}
+	
+	@Test
+	public void testGetSynopsis() {
+		
+	}
+	
+	@Test
+	public void testSetSynopsis() {
+		
+	}
+	
+	@Test
+	public void testGetPublishDate() {
+		
+	}
+	
+	@Test
+	public void testGetPublishDateString() {
+		
+	}
+	
+	@Test
+	public void testSetPublishDate() {
+		
+	}
+	
+	@Test
+	public void testGetStartingFragmentID() {
+		
+	}
+	
+	@Test
+	public void testSetStartingFragmentID() {
+		
+	}
+	
+	@Test
+	public void testGetSID() {
+		
+	}
+	
+	@Test
+	public void testSetSID() {
+		
+	}
+	
+	@Test
+	public void testGetPublishState() {
+		
+	}
+	
+	@Test
+	public void testSetPublishState() {
+		
+	}
+	
+	@Test
+	public void testToString() {
+		
+	}
+}
