330b08947d8ff5be8fcde66c364cfcf882650731
Alexander Cheung <atcheung@ualberta.ca>
2013-11-27 16:49:26 -0700

    Test creation and initialization

--- src/story/book/test/AudioIllustrationTest.java
+++ src/story/book/test/AudioIllustrationTest.java
@@ -2,14 +2,18 @@ package story.book.test;
 
 import org.junit.Test;
 
+import story.book.model.AudioIllustration;
 import story.book.view.Dashboard;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class AudioIllustrationTest extends ActivityInstrumentationTestCase2
 <story.book.view.Dashboard> {
 
+	private AudioIllustration audioIllustration;
+	
 	public AudioIllustrationTest() {
 		super(Dashboard.class);
+		audioIllustration = new AudioIllustration(null);
 	}
 	
 	@Test
