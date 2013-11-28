6315b35a63b152ee87a9ca982cdcd140e5f1a8f3
Alexander Cheung <atcheung@ualberta.ca>
2013-11-27 17:29:42 -0700

    VideoIllustration tests

--- src/story/book/test/VideoIllustrationTest.java
+++ src/story/book/test/VideoIllustrationTest.java
@@ -2,19 +2,40 @@ package story.book.test;
 
 import org.junit.Test;
 
+import story.book.model.VideoIllustration;
 import story.book.view.Dashboard;
+import android.net.Uri;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class VideoIllustrationTest extends ActivityInstrumentationTestCase2
 <story.book.view.Dashboard> {
 
+	private VideoIllustration videoIllustration;
+	
 	public VideoIllustrationTest() {
 		super(Dashboard.class);
+		videoIllustration = new VideoIllustration(Uri.parse("http://www.google.com"));
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreation() {
+		assertNotNull(videoIllustration);
+	}
+	
+	@Test
+	public void testGetContent() {
+		assertEquals(videoIllustration.getContent(), Uri.parse("http://www.google.com").getLastPathSegment());
+	}
+	
+	@Test
+	public void testSetContent() {
+		videoIllustration.setContent("http://www.google.ca");
+		assertEquals(videoIllustration.getContent(), "http://www.google.ca");
+	}
+	
+	@Test
+	public void testGetView() {
+		assertNotNull(videoIllustration.getView(null, false, getActivity()));
 	}
 
 }
