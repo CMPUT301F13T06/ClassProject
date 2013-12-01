27d67263f2653b945e8e2567ee2367a06ee5c2e4
Alexander Cheung <atcheung@ualberta.ca>
2013-12-01 14:56:46 -0700

    Main constructor for test, does not work

--- src/story/book/test/ImageIllustrationTest.java
+++ src/story/book/test/ImageIllustrationTest.java
@@ -1,15 +1,38 @@
 package story.book.test;
 
+import java.io.File;
+
 import org.junit.Test;
 
+import story.book.model.Annotation;
+import story.book.model.ImageIllustration;
+import story.book.model.TextIllustration;
 import story.book.view.Dashboard;
+import android.net.Uri;
+import android.os.Environment;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class ImageIllustrationTest extends ActivityInstrumentationTestCase2
 <story.book.view.Dashboard> {
 
+	Uri sampleImageUri;
+	File sampleImageFile;
+	ImageIllustration imageIllustration;
+	
+	// Errors exist in this test related to the fact that ImageIllustration is
+	// tightly coupled by many dependencies related to system io
 	public ImageIllustrationTest() {
 		super(Dashboard.class);
+
+	    sampleImageFile = new File(Environment.getExternalStorageDirectory()
+	         + File.separator
+	         + "testtempdump"
+	         + File.separator
+	         + "notreallya.png");
+	    sampleImageFile.mkdirs();
+	    sampleImageUri = Uri.parse("/sdcard/testtempdump/notreallya.png");
+	    
+	    imageIllustration = new ImageIllustration(sampleImageUri, sampleImageUri);
 	}
 	
 	@Test
